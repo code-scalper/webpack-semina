3468525e91b150fa37f18b0d8452678786089f12
HCC\junwoo.l <tigerbaram@gmail.com>
2023-05-26 10:04:21 +0900

    d

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import cScore from "./data/c";
 
 import calculation from "./function/calculateAverage";
 
-const result = calculation(aScore, bScore, cScore);
+const result = calculation([aScore, bScore, cScore]);
 document.querySelector("#result").innerHTML = `결과값 : ${result}`;
